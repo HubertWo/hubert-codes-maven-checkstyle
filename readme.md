8574fbdc8637d1181021a3c03d82a5745d92decd
Hubert Wo <2518652+HubertWo@users.noreply.github.com>
2022-04-16 18:59:57 +0200

    Maven project

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Checking code style with Maven and Checkstyle
+This repository contains source code for blog post [Checking code style with Maven and Checkstyle](https://hubert.codes/posts/devops/maven-code-quality-with-checkstyle/). \
+If you found this code or post useful do not forget to leave a ⭐️  Thanks! :)
---
+++ src/main/java/codes/hubertwo/maven/checkstyle/App.java
@@ -0,0 +1,19 @@
+package codes.hubertwo.maven.checkstyle;
+
+/**
+ * Messy
+ *          code
+ *          makes
+*           programmers
+ * life
+ *  <p>harder.</p>
+ */
+public class App {
+
+    final static public String myConst = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ";
+
+    public static void main(String[] args) {
+        System.out.println(myConst);
+        System.out.println("Please fix me :)");
+    }
+}
